d3a6029464a3d1a7c2b7001686155627cd1adfb3
Bruno <bruno.masunaga@aluno.ufabc.edu.br>
2018-08-04 15:15:25 -0300

    Adicionado Merge e Quick

--- sorths/src/sorting.hs
+++ sorths/src/sorting.hs
@@ -9,13 +9,13 @@ chamadaBubble :: (Ord a) => [a] -> [a]
 chamadaBubble (x1:x2:xs) = if x1 <= x2
                            then x1:(bubbleSort $ x2:xs)
                            else x2:(bubbleSort $ x1:xs)
-chamadaBubble xs         = xs
                       
 bubbleSort :: (Ord a) => [a] -> [a]
-bubbleSort xs  = if ordenado chamadaRec
-                 then chamadaRec 
-                 else bubbleSort chamadaRec
-                    where chamadaRec = chamadaBubble xs
+bubbleSort (x1:x2:xs)  = if ordenado chamadaRec
+                         then chamadaRec 
+                         else bubbleSort chamadaRec
+                            where chamadaRec = chamadaBubble (x1:x2:xs)
+bubbleSort xs          = xs
                          
 -- Insertion Sort
 inserir :: (Ord a) => a -> [a] -> [a]
@@ -25,9 +25,8 @@ inserir x (y:ys) = if x <= y
                    else y:(inserir x ys)
 
 insertionSort :: (Ord a) => [a] -> [a]
-insertionSort []     = []
-insertionSort [x]    = [x]
-insertionSort (x:xs) = inserir x $ insertionSort xs
+insertionSort (x1:x2:xs) = inserir x1 $ insertionSort (x2:xs)
+insertionSort xs         = xs
 
 -- Selection Sort
 remover :: (Ord a) => a -> [a] -> [a]
@@ -37,14 +36,40 @@ remover x (y:ys) = if x == y
                    else y:(remover x ys)
 
 selectionSort :: (Ord a) => [a] -> [a]
-selectionSort []  = []
-selectionSort [x] = [x]
-selectionSort xs  = minimo:(selectionSort $ remover minimo xs) 
-                       where minimo = minimum xs
-
+selectionSort (x1:x2:xs) = minimo:(selectionSort $ remover minimo (x1:x2:xs)) 
+                              where minimo = minimum (x1:x2:xs)
+selectionSort xs         = xs
+                              
 -- Counting Sort
 -- Merge Sort
+intercalar :: (Ord a) => [a] -> [a] -> [a]
+intercalar [] []         = []
+intercalar [] ys         = ys
+intercalar xs []         = xs
+intercalar (x:xs) (y:ys) = if x <= y
+                           then x:(intercalar xs (y:ys))
+                           else y:(intercalar (x:xs) ys)
+
+dividir :: (Ord a) => [a] -> ([a],[a])
+dividir []  = ([], [])
+dividir [x] = ([x], [])
+dividir xs  = (take metade xs, drop metade xs)
+                 where metade = div (length xs) 2
+                 
+mergeSort :: Ord a => [a] -> [a]
+mergeSort (x1:x2:xs) = intercalar (mergeSort esq) (mergeSort dir)
+                          where (esq, dir) = dividir (x1:x2:xs)
+mergeSort xs         = xs
+
 -- Quick Sort
+dividirPivot :: (Ord a) => [a] -> ([a],[a])
+dividirPivot (x:xs)  = (filter (<= x) xs, filter (> x) xs)
+
+quickSort :: (Ord a) => [a] -> [a]
+quickSort (x1:x2:xs) = quickSort pivotEsq ++ [x1] ++ quickSort pivotDir
+                          where (pivotEsq, pivotDir) = dividirPivot (x1:x2:xs)
+quickSort xs         = xs
+
 -- Heap Sort
 -- Bucket Sort
 -- Radix Sort
