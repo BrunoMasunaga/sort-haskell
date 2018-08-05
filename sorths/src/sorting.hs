a184ca34868bac729a655f248968fa5a7f2153b4
Bruno <bruno.masunaga@aluno.ufabc.edu.br>
2018-08-05 00:08:21 -0300

    adicionado Counting

--- sorths/src/sorting.hs
+++ sorths/src/sorting.hs
@@ -1,6 +1,5 @@
 import Data.List
-
--- Bubble Sort
+---------------------------------- Bubble Sort ---------------------------------
 ordenado :: (Ord a) => [a] -> Bool
 ordenado (x1:x2:xs) = if x1 <= x2
                       then ordenado $ x2:xs
@@ -18,8 +17,7 @@ bubbleSort (x1:x2:xs)  = if ordenado chamadaRec
                          else bubbleSort chamadaRec
                             where chamadaRec = chamadaBubble $ x1:x2:xs
 bubbleSort xs          = xs
-                         
--- Insertion Sort
+-------------------------------- Insertion Sort --------------------------------
 inserir :: (Ord a) => a -> [a] -> [a]
 inserir x []     = [x]
 inserir x (y:ys) = if x <= y
@@ -29,8 +27,7 @@ inserir x (y:ys) = if x <= y
 insertionSort :: (Ord a) => [a] -> [a]
 insertionSort (x1:x2:xs) = inserir x1 $ insertionSort $ x2:xs
 insertionSort xs         = xs
-
--- Selection Sort
+-------------------------------- Selection Sort --------------------------------
 remover :: (Ord a) => a -> [a] -> [a]
 remover x []     = []
 remover x (y:ys) = if x == y
@@ -41,8 +38,7 @@ selectionSort :: (Ord a) => [a] -> [a]
 selectionSort (x1:x2:xs) = minimo:(selectionSort $ remover minimo (x1:x2:xs)) 
                               where minimo = minimum $ x1:x2:xs
 selectionSort xs         = xs
-                              
--- Merge Sort
+---------------------------------- Merge Sort ----------------------------------
 intercalar :: (Ord a) => [a] -> [a] -> [a]
 intercalar [] []         = []
 intercalar [] ys         = ys
@@ -61,8 +57,7 @@ mergeSort :: Ord a => [a] -> [a]
 mergeSort (x1:x2:xs) = intercalar (mergeSort esq) (mergeSort dir)
                           where (esq, dir) = dividir $ x1:x2:xs
 mergeSort xs         = xs
-
--- Quick Sort
+---------------------------------- Quick Sort ----------------------------------
 dividirPivot :: (Ord a) => [a] -> ([a],[a])
 dividirPivot (x:xs)  = (filter (<= x) xs, filter (> x) xs)
 
@@ -70,8 +65,7 @@ quickSort :: (Ord a) => [a] -> [a]
 quickSort (x1:x2:xs) = quickSort pivotEsq ++ [x1] ++ quickSort pivotDir
                           where (pivotEsq, pivotDir) = dividirPivot $ x1:x2:xs
 quickSort xs         = xs
-
--- Counting Sort
+--------------------------------- Counting Sort --------------------------------
 tamanhoAuxiliar :: [Int] -> Int
 tamanhoAuxiliar xs = (maximum xs)-(minimum xs)
 
@@ -82,20 +76,24 @@ construirAuxiliar :: [Int] -> [Int]
 construirAuxiliar xs = [contarOcorrencias (x+min) xs | x <- [0..tAux]] 
                           where min = minimum xs
                                 tAux = tamanhoAuxiliar xs
-                                
+
+incOcorrencias :: [Int] -> [Int]
+incOcorrencias []         = []
+incOcorrencias (x1:x2:xs) = x1:(incOcorrencias $ (x1+x2):xs)
+
 add :: Int -> Int -> [Int] -> [Int]
 add _ 0 xs = xs
 add x i xs = add x (i-1) (xs ++ [x])  
 
 countingSort :: [Int] -> [Int]
-countingSort (x1:x2:xs) = concat [add (y+min) (auxiliar!!y) [] | y <- [0..tAux]]
-                             where auxiliar = construirAuxiliar $ x1:x2:xs
-                                   min      = minimum $ x1:x2:xs
-                                   tAux     = tamanhoAuxiliar $ x1:x2:xs
-countingSort xs         = xs
-
--- Heap Sort
--- Bucket Sort (Utilizando 10 buckets)
+countingSort []  = []
+countingSort [x] = [x]
+countingSort xs  = concat [add (y+min) (auxiliar!!y) [] | y <- [0..tAux]]
+                       where auxiliar = construirAuxiliar xs
+                             min      = minimum xs
+                             tAux     = tamanhoAuxiliar xs
+----------------------------------- Heap Sort ----------------------------------
+---------------------- Bucket Sort (Utilizando 10 buckets) ---------------------
 divisor :: [Int] -> Int
 divisor xs = (div ((maximum xs) + 1) 10) + 1
 
@@ -116,5 +114,4 @@ ordenarBuckets (xs:xss) = (insertionSort xs):(ordenarBuckets xss)
 bucketSort :: [Int] -> [Int]
 bucketSort (x1:x2:xs) = concat $ ordenarBuckets $ criarBuckets (x1:x2:xs)
 bucketSort xs         = xs
-
--- Radix Sort
+---------------------------------- Radix Sort ----------------------------------
